6697d213e68e06851af52af1849abc8a5b600554
Kleber Silveira <kleberbastosdasilveira@gmail.com>
2020-11-02 08:28:01 -0300

    Ultima Atualização

--- src/Mundipagg.Aplication.API/Startup.cs
+++ src/Mundipagg.Aplication.API/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Mundipagg.Infra.CrossCutting;
 using Mundipagg.Infra.CrossCutting.InversionOfControl;
 using Mundipagg.Infra.Data.Repository;
 using Mundipagg.Services.API.Configurations;
@@ -13,10 +12,6 @@ namespace Mundipagg.Services.API
     public class Startup
     {
         public IConfiguration Configuration { get; }
-        //public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
         public Startup(IHostEnvironment hostEnvironment)
         {
             var builder = new ConfigurationBuilder()
